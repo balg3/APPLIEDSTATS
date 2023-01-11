f1667cbe483c9371f13fb25e834f8cee737b2b4d
balg3 <90233309+balg3@users.noreply.github.com>
2023-01-11 10:39:33 -0800

    Add files via upload

--- final1code.R
+++ final1code.R
@@ -3,15 +3,30 @@ View(fishdata)
 attach(fishdata)
 library(ggplot2)
 library(tidyverse)
-ggplot(fishdata)+
+library(dplyr)
+
+
+findoutlier <- function(x) {
+  return(x < quantile(x, .25) - 1.5*IQR(x) | x > quantile(x, .75) + 1.5*IQR(x))
+}
+
+df1 <- data.frame(ChosBodLen, RejBodLen)
+
+  first <- ggplot(df1)+
   coord_flip()+
-  geom_boxplot(width = .25, aes(y=ChosBodLen), fill='blue')+
-  geom_boxplot(width = .25, aes(y=RejBodLen), position= position_nudge(x=-.5), fill='red')+
+  scale_fill_manual(values = c("Chosen" = "#42d4f5","Rejected" = "#ed736d"))+
+  geom_boxplot(width = .25, aes(y=ChosBodLen, fill = "Chosen"))+
+  geom_boxplot(width = .25, aes(y=RejBodLen, fill = "Rejected"), position = position_nudge(x=-.5))+
   theme_light()+
   labs(
-    y = "Lenght of Fish"
+    y = "Centimeters",
+    title = "Length of Fish",
+    fill = "Mating"
   )+
   theme(
-    axis.text.y = (element_blank())
+    axis.text.y = (element_blank()),
+    plot.title = (element_text(hjust = 0.5))
   )
+
+first
 
