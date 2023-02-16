7684dd7512a96c1d4de0fbcf97514585b645a4c1
balg3 <90233309+balg3@users.noreply.github.com>
2023-02-16 10:39:53 -0800

    Add files via upload

--- snakes.R
+++ snakes.R
@@ -1,9 +1,11 @@
 setwd("C:/Users/torrealb000/Desktop/Applied Stats/Snakes")
 library(ggplot2)
 library(tidyverse)
-snake <- read.csv("C:/Users/torrealb000/Desktop/Applied Stats/Snakes/snake - Sheet1.csv", row.names=1)
+library(GGally)
+snake <- read.csv("C:/Users/torrealb000/Desktop/Applied Stats/Snakes/snake.csv", row.names=1)
+attach(snake)
 
-
+#NUMBER ONE
 
 ggplot(snake, aes(x=Lat.Index))+
   geom_density(color="blue", size=0.75)+
@@ -23,8 +25,48 @@ ggplot(snake, aes(x=Lat.Index))+
     plot.title = element_text(face = "bold", size = 12),
     plot.background = element_rect(fill = "white")
   )+
-  theme_light()
+  teme_light()
 
-t.test()
-  
-  
+t.test(Lat.Index, alternative = c("two.sided"), mu = 0.5)
+
+sd(Lat.Index)
+
+#NUMBER TWO 
+
+pairs(snake)
+
+ggplot(snake, aes(x=Length, y=Lat.Index, color = Gender))+
+  geom_point()+
+  theme_light()+
+  labs(
+    title = "Laterality Index as a function of Length with Gender",
+    x = "Length",
+    y = "Laterality Index"
+  )+
+  theme(
+    plot.title = element_text(face = "bold", size = 12),
+    plot.background = element_rect(fill = "white")
+  )+
+  scale_color_manual(values = c('#e34685', '#466ee3'), Gender)
+
+
+d <- ggplot(snake, aes(x=Length, y=Lat.Index, color = Age))+
+  geom_point()+
+  theme_light()+
+  labs(
+    title = "Laterality Index as a function of Length with Age",
+    x = "Length",
+    y = "Laterality Index",
+    color = "Age"
+  )+
+  theme(
+    plot.title = element_text(face = "bold", size = 12),
+    plot.background = element_rect(fill = "white")
+  )+
+  scale_color_manual(values = c('#8f42d6', '#3acf55'), Age)
+d
+
+
+lm(Lat.Index~Length)
+
+ggpairs(snake)
