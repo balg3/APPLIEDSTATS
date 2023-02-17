9a075211626e2b0876cffefa47fba7017d9330eb
balg3 <90233309+balg3@users.noreply.github.com>
2023-02-17 01:44:00 -0800

    Add files via upload

--- snakes.R
+++ snakes.R
@@ -1,8 +1,9 @@
-setwd("C:/Users/torrealb000/Desktop/Applied Stats/Snakes")
+setwd("C:/Users/upist/Desktop/Applied Stats/snkaesk")
 library(ggplot2)
 library(tidyverse)
 library(GGally)
-snake <- read.csv("C:/Users/torrealb000/Desktop/Applied Stats/Snakes/snake.csv", row.names=1)
+library(dplyr)
+snake <- read.csv("C:/Users/upist/Desktop/Applied Stats/snkaesk/snakes.csv", row.names=1)
 attach(snake)
 
 #NUMBER ONE
@@ -17,15 +18,14 @@ ggplot(snake, aes(x=Lat.Index))+
     color = "blue", linetype="dashed", size=0.75
   )+
   labs(
-      x = "Laterality Index",
-      y = "Count",
-      title = "Laterality Index Density Plot"
+    x = "Laterality Index",
+    y = "Count",
+    title = "Laterality Index Density Plot"
   )+
   theme(
     plot.title = element_text(face = "bold", size = 12),
     plot.background = element_rect(fill = "white")
-  )+
-  teme_light()
+  )
 
 t.test(Lat.Index, alternative = c("two.sided"), mu = 0.5)
 
@@ -33,9 +33,7 @@ sd(Lat.Index)
 
 #NUMBER TWO 
 
-pairs(snake)
-
-ggplot(snake, aes(x=Length, y=Lat.Index, color = Gender))+
+w <- ggplot(snake, aes(x=Length, y=Lat.Index, color = Gender))+
   geom_point()+
   theme_light()+
   labs(
@@ -47,8 +45,9 @@ ggplot(snake, aes(x=Length, y=Lat.Index, color = Gender))+
     plot.title = element_text(face = "bold", size = 12),
     plot.background = element_rect(fill = "white")
   )+
-  scale_color_manual(values = c('#e34685', '#466ee3'), Gender)
-
+  scale_color_manual(values = c('#e34685', '#466ee3'), Gender)+
+  guides(color=guide_legend("Gender"))
+w
 
 d <- ggplot(snake, aes(x=Length, y=Lat.Index, color = Age))+
   geom_point()+
@@ -56,17 +55,79 @@ d <- ggplot(snake, aes(x=Length, y=Lat.Index, color = Age))+
   labs(
     title = "Laterality Index as a function of Length with Age",
     x = "Length",
-    y = "Laterality Index",
-    color = "Age"
+    y = "Laterality Index"
   )+
   theme(
     plot.title = element_text(face = "bold", size = 12),
     plot.background = element_rect(fill = "white")
   )+
-  scale_color_manual(values = c('#8f42d6', '#3acf55'), Age)
+  scale_color_manual(values = c('#8f42d6', '#3acf55'), Age)+
+  guides(color=guide_legend("Age"))
 d
 
+len <- lm(Lat.Index~Length)
+summary(len)
 
-lm(Lat.Index~Length)
+age <- lm(Lat.Index~Age)
+summary(age)
+
+gen <- lm(Lat.Index~Gender)
+summary(gen)
+
+lenage <- lm(Lat.Index~Length+Age)
+summary(lenage)
+
+lengen <- lm(Lat.Index~Length+Gender)
+summary(lengen)
+
+lenagegen <- lm(Lat.Index~Length+Age+Gender)
+summary(lenagegen)
+
+agegen <- lm(Lat.Index~Age+Gender)
+summary(agegen)
+
+bruh <- lm(Lat.Index~Length+Age+Length:Gender)
+summary(bruh)
 
 ggpairs(snake)
+
+#NUMBER THREE
+
+t.test(Length ~ Age, alternative = "two.sided")
+t.test(Length ~ Gender, alternative = "two.sided")
+
+aggregate(Lat.Index, list(Age), FUN=sd) 
+aggregate(Lat.Index, list(Gender), FUN=sd) 
+aggregate(Lat.Index, list(Age), FUN=length) 
+aggregate(Lat.Index, list(Gender), FUN=length) 
+
+
+qq1 <- ggplot(snake, aes(sample = Length, color = factor(Age)))+
+  stat_qq()+
+  theme(
+    plot.title = element_text(face = "bold", size = 12),
+    plot.background = element_rect(fill = "white")
+  )+
+  scale_color_manual(values = c('#8f42d6', '#3acf55'), Age)+
+  labs(
+    x = "Theoretical",
+    y = "Sample",
+    title = "Q-Q Probability Plot for Age"
+  )+
+  guides(color=guide_legend("Age"))
+qq1
+
+qq2 <- ggplot(snake, aes(sample = Length, color = factor(Gender)))+
+  stat_qq()+
+  theme(
+    plot.title = element_text(face = "bold", size = 12),
+    plot.background = element_rect(fill = "white")
+  )+
+  scale_color_manual(values = c('#e34685', '#466ee3'), Gender)+
+  labs(
+    x = "Theoretical",
+    y = "Sample",
+    title = "Q-Q Probability Plot for Gender"
+  )+
+  guides(color=guide_legend("Gender"))
+qq2
