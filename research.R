bb1c04e20aa3be918fb174a98ee2b0bf015c1e18
balg3 <90233309+balg3@users.noreply.github.com>
2023-03-01 10:39:41 -0800

    Add files via upload

--- research.R
+++ research.R
@@ -31,3 +31,22 @@ lm1 <- lm(overweight~smoke)
 summary(lm1)
 
 #NUMBA4
+#NUMBA5
+#NUMBA6
+#NUMBA7 
+PervsObs <- data.frame(Predicted = predict(lm1),
+                       Observed = Exercise$overweight)
+
+fig2<-ggplot(PervsObs, aes(Predicted, Observed))+
+  geom_point()+
+  labs(
+    x = "Predicted Overweight Prevalence",
+    y = "Observed Overweight Prevalence",
+    title = "Predicted Overweight Prevalence From Smoking Prevalence Against Observed"
+  )+
+  theme(
+    plot.title = element_text(face = "bold")
+  )
+
+fig2
+
