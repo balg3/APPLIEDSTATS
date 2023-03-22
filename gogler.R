cc8ad10a87f21900122051212f8d51ea55af6e8a
balg3 <90233309+balg3@users.noreply.github.com>
2023-03-22 10:40:04 -0700

    Add files via upload

--- gogler.R
+++ gogler.R
@@ -1,5 +1,6 @@
 setwd("C:/Users/torrealb000/Desktop/Applied Stats/day2log")
 library(readxl)
+library(ggplot2)
 FirstYearGPA <- read_excel("FirstYearGPA.xlsx", 
                            col_types = c("numeric", "numeric", "numeric", 
                                          "numeric", "text", "text", "text"))
@@ -7,7 +8,17 @@ attach(FirstYearGPA)
 
 #a
 
-baller <-t.test(GPA~FirstGen)
-baller
+GPAfirstgen <-t.test(GPA~FirstGen)
+GPAfirstgen
 
-  
+tapply(GPA,FirstGen, FUN = sd)
+tapply(GPA,FirstGen, FUN = length)
+
+ggplot(FirstYearGPA, aes(x = factor(FirstGen), y = GPA)) +
+  geom_boxplot()+
+  theme_bw()+
+  labs(
+    title = "GPA Distribution by First Gen",
+    y = "GPA",
+    x = "First Gen"
+  )
