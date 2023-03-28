d550422fc52c4bec3d8dfe476fc4167cfd1538c4
balg3 <90233309+balg3@users.noreply.github.com>
2023-03-28 00:01:09 -0700

    Update gogler.R

--- gogler.R
+++ gogler.R
@@ -22,3 +22,47 @@ ggplot(FirstYearGPA, aes(x = factor(FirstGen), y = GPA)) +
     y = "GPA",
     x = "First Gen"
   )
+######################################
+setwd("C:/Users/upist/Desktop/Applied Stats/otherstuff")
+library(readxl)
+library(ggplot2)
+FirstYearGPA <- read_excel("FirstYearGPA.xls", 
+                           col_types = c("numeric", "numeric", "numeric", 
+                                         "numeric", "text", "text", "text"))
+FirstYearGPA2 <- read_excel("FirstYearGPA.xls", 
+                            col_types = c("numeric", "numeric", "numeric", 
+                                          "numeric", "numeric", "numeric", "numeric"))
+attach(FirstYearGPA)
+attach(FirstYearGPA2)
+
+#a
+
+baller <-t.test(GPA~FirstGen)
+baller
+
+jager <-lm(GPA~factor(FirstGen))
+summary(jager)
+summary(lm(GPA~factor(FirstGen)))
+
+tapply(GPA,FirstGen, FUN = sd)
+tapply(GPA,FirstGen, FUN = length)
+
+ggplot(FirstYearGPA, aes(x = factor(FirstGen), y = GPA)) +
+  geom_boxplot()+
+  theme_bw()+
+  labs(
+    title = "GPA Distribution by First Gen",
+    y = "GPA",
+    x = "First Gen"
+  )
+
+ggplot(jager, aes(x = .fitted, y = .resid)) +
+  geom_point()+
+  theme_bw()+
+  labs(
+    title = "Residual Plot of FirstGen to Predict GPA",
+    y = "Residual",
+    x = "Predicted GPA"
+  )
+
+summary(glm(FirstGen ~ GPA, family = binomial(link = "logit")))
