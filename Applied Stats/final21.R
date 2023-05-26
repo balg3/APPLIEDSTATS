4568735453993752051fb8edaa1ae98c40549b0d
balg3 <90233309+balg3@users.noreply.github.com>
2023-05-26 10:40:31 -0700

    Add files via upload

--- Applied Stats/final21.R
+++ Applied Stats/final21.R
@@ -6,20 +6,50 @@ GUKR71FL <- read_dta("GUKR71FL.DTA")
 View(GUKR71FL)
 attach(GUKR71FL)
 
-hemoweal <- ggplot(GUKR71FL, aes(v191, hw56, color = factor(v131)))+
-  geom_point()+
+hemowealeth <- ggplot(GUKR71FL, aes(v191, hw56, color = factor(v131)))+
+  geom_point(alpha = 0.45)+
   labs(
-    x = "Hemoglobin in centigram/dl",
-    y = "Wealth Index",
-    title = "Hemoglobin as a function of Wealth Index",
+    y = "Hemoglobin in Decigram/Deciliter",
+    x = "Wealth Index",
+    title = "Hemoglobin as a Function of Wealth Index",
     color = "Ethnicity"
   )+
-  scale_color_manual(values = c("1"="#a134eb",
-                                "2"="#5634eb",
-                                "3"="#eb34c3",
+  scale_color_manual(values = c("1"="#34ebe2",
+                                "2"="#ed4edd",
+                                "3"="#5634eb",
                                 "4"="#eb5334",
-                                "5"="#40eb34",
-                                "8"="#34ebe2",))
+                                "6" ="orange",
+                                "8"="#40eb34"))+
+  theme_bw()
+  
+hemowealeth
+
+GUKR71FL$Maya <- NA
+GUKR71FL$Maya <- ifelse(GUKR71FL$v131 == 1, GUKR71FL$hw56, NA)
+GUKR71FL$Mestizo <- NA
+GUKR71FL$Mestizo <- ifelse(GUKR71FL$v131 == 2, GUKR71FL$hw56, NA)
+GUKR71FL$Garifuna <- NA
+GUKR71FL$Garifuna <- ifelse(GUKR71FL$v131 == 3, GUKR71FL$hw56, NA)
+GUKR71FL$Xinca <- NA
+GUKR71FL$Xinca <- ifelse(GUKR71FL$v131 == 4, GUKR71FL$hw56, NA)
+
+
+
+
+hemoweal <- ggplot(GUKR71FL, aes(v191, hw56, color = factor(v131)))+
+  geom_point(alpha = 0.4)+
+  labs(
+    y = "Hemoglobin in Decigram/Deciliter",
+    x = "Wealth Index",
+    title = "Hemoglobin as a Function of Wealth Index",
+    color = "Ethnicity"
+  )+
+  scale_color_manual(values = c("1"="#34ebe2",
+                                "2"="#5634eb",
+                                "3"="#40eb34",
+                                "4"="#eb5334",
+                                "6" ="orange",
+                                "8"="#a134eb"))
+
 hemoweal
-
-
+        
